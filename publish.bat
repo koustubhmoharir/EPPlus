00a57196083e62fb6014f4d77c3930b997c8b78b
chetanQP <chetan@quantumphinance.com>
2017-10-11 16:18:04 +0530

    master: Changes for publishing to nuget

--- publish.bat
+++ publish.bat
@@ -1,8 +1,8 @@
 mkdir EPPlus\bin\package
 cd EPPlus\bin\package
 del * /q
-..\..\..\nuget pack ..\..\EPPlusSK.nuspec
-..\..\..\nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
+..\..\..\.nuget\nuget pack ..\..\EPPlusSK.nuspec
+..\..\..\.nuget\nuget push *.nupkg -Source https://www.nuget.org/api/v2/package
 cd..
 cd..
 cd..
