9745f15b451658a97a5ea2228bd502c62e8bf469
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-18 15:07:37 +0000

    Update table tests with escaping test

--- EPPlusTest/Table/ExcelTableCollectionTest.cs
+++ EPPlusTest/Table/ExcelTableCollectionTest.cs
@@ -2,6 +2,7 @@ using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml;
 using OfficeOpenXml.Table;
+using System.Xml;
 
 namespace EPPlusTest.Table
 {
@@ -17,7 +18,6 @@ namespace EPPlusTest.Table
                 var range = ws.Cells["A1:C5"];
                 
                 // "A1" should be allowed in stable but might be disallowed in dotnetport if it looks like an address
-                // Actually, let's try something that is definitely a valid name in stable but potentially invalid in Excel
                 ws.Tables.Add(range, "A1");
                 Assert.AreEqual("A1", ws.Tables[0].Name);
             }
@@ -44,6 +44,29 @@ namespace EPPlusTest.Table
                 var ws = pck.Workbook.Worksheets.Add("Sheet1");
                 var range = ws.Cells["A1:C5"];
                 ws.Tables.Add(range, "1Table");
+            }
+        }
+
+        [TestMethod]
+        public void TableNameEscapingTest()
+        {
+            using (var pck = new ExcelPackage())
+            {
+                var ws = pck.Workbook.Worksheets.Add("Sheet1");
+                var range = ws.Cells["A1:C5"];
+                
+                // In stable, displayName is cleaned using regex [^\w\.-_] -> _
+                // '#' is outside the range .-_
+                string name = "Table_Name_With_#";
+                var table = ws.Tables.Add(range, name);
+                
+                Assert.AreEqual(name, table.Name);
+                
+                var doc = table.TableXml;
+                var displayName = doc.DocumentElement.GetAttribute("displayName");
+                
+                // In stable, # is replaced by _
+                Assert.AreEqual("Table_Name_With__", displayName);
             }
         }
     }
