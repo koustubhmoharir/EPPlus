956c0fa95aa42bdcbad8b213f119aa37738880b4
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-18 18:31:14 +0000

    Add ExcelChartAxis gridlines implementation and tests to stable branch

--- EPPlusTest/Drawing/Chart/ExcelChartAxisTest.cs
+++ EPPlusTest/Drawing/Chart/ExcelChartAxisTest.cs
@@ -1,11 +1,8 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml;
 using OfficeOpenXml.Drawing.Chart;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Reflection;
 using System.Xml;
 
 namespace EPPlusTest.Drawing.Chart
@@ -21,7 +18,27 @@ namespace EPPlusTest.Drawing.Chart
             var xmlDoc = new XmlDocument();
             var xmlNsm = new XmlNamespaceManager(new NameTable());
             xmlNsm.AddNamespace("c", ExcelPackage.schemaChart);
-            axis = new ExcelChartAxis(xmlNsm, xmlDoc.CreateElement("axis"));
+            xmlNsm.AddNamespace("a", ExcelPackage.schemaDrawings);
+
+            var ctor = typeof(ExcelChartAxis).GetConstructor(
+                BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public,
+                null,
+                new Type[] { typeof(XmlNamespaceManager), typeof(XmlNode) },
+                null
+            );
+            axis = (ExcelChartAxis)ctor.Invoke(new object[] { xmlNsm, xmlDoc.CreateElement("axis") });
+        }
+
+        private XmlNode GetTopNode(ExcelChartAxis target)
+        {
+            var prop = typeof(XmlHelper).GetProperty("TopNode", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            return (XmlNode)prop.GetValue(target);
+        }
+
+        private XmlNamespaceManager GetNameSpaceManager(ExcelChartAxis target)
+        {
+            var prop = typeof(XmlHelper).GetProperty("NameSpaceManager", BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+            return (XmlNamespaceManager)prop.GetValue(target);
         }
 
         [TestMethod]
@@ -64,6 +81,55 @@ namespace EPPlusTest.Drawing.Chart
         {
             axis.MaxValue = 1.2e-6;
             Assert.AreEqual(axis.MaxValue, 1.2e-6);
+        }
+
+        [TestMethod]
+        public void Gridlines_GetMajor_IsNotNullAndCreatesNode()
+        {
+            var major = axis.MajorGridlines;
+            Assert.IsNotNull(major);
+            Assert.IsNotNull(GetTopNode(axis).SelectSingleNode("c:majorGridlines", GetNameSpaceManager(axis)));
+            Assert.AreSame(major, axis.MajorGridlines);
+        }
+
+        [TestMethod]
+        public void Gridlines_GetMinor_IsNotNullAndCreatesNode()
+        {
+            var minor = axis.MinorGridlines;
+            Assert.IsNotNull(minor);
+            Assert.IsNotNull(GetTopNode(axis).SelectSingleNode("c:minorGridlines", GetNameSpaceManager(axis)));
+            Assert.AreSame(minor, axis.MinorGridlines);
+        }
+
+        [TestMethod]
+        public void Gridlines_RemoveGridlinesDefault_RemovesBoth()
+        {
+            var major = axis.MajorGridlines;
+            var minor = axis.MinorGridlines;
+            Assert.IsNotNull(GetTopNode(axis).SelectSingleNode("c:majorGridlines", GetNameSpaceManager(axis)));
+            Assert.IsNotNull(GetTopNode(axis).SelectSingleNode("c:minorGridlines", GetNameSpaceManager(axis)));
+
+            axis.RemoveGridlines();
+
+            Assert.IsNull(GetTopNode(axis).SelectSingleNode("c:majorGridlines", GetNameSpaceManager(axis)));
+            Assert.IsNull(GetTopNode(axis).SelectSingleNode("c:minorGridlines", GetNameSpaceManager(axis)));
+        }
+
+        [TestMethod]
+        public void Gridlines_RemoveGridlinesSelective_RemovesExpected()
+        {
+            // Case 1: removeMajor=true, removeMinor=false
+            var major = axis.MajorGridlines;
+            var minor = axis.MinorGridlines;
+            axis.RemoveGridlines(true, false);
+            Assert.IsNull(GetTopNode(axis).SelectSingleNode("c:majorGridlines", GetNameSpaceManager(axis)));
+            Assert.IsNotNull(GetTopNode(axis).SelectSingleNode("c:minorGridlines", GetNameSpaceManager(axis)));
+
+            // Case 2: removeMajor=false, removeMinor=true
+            major = axis.MajorGridlines; // recreate major
+            axis.RemoveGridlines(false, true);
+            Assert.IsNotNull(GetTopNode(axis).SelectSingleNode("c:majorGridlines", GetNameSpaceManager(axis)));
+            Assert.IsNull(GetTopNode(axis).SelectSingleNode("c:minorGridlines", GetNameSpaceManager(axis)));
         }
     }
 }
