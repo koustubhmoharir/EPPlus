99fe67ee16cfa63b8f009bd27f4cc9d44381f4db
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-18 15:06:12 +0000

    Update Differences.md and PortingProgress.md with ExcelTableCollection status

--- EPPlusTest/Table/ExcelTableCollectionTest.cs
+++ EPPlusTest/Table/ExcelTableCollectionTest.cs
@@ -16,10 +16,26 @@ namespace EPPlusTest.Table
                 var ws = pck.Workbook.Worksheets.Add("Sheet1");
                 var range = ws.Cells["A1:C5"];
                 
-                // "A1" should be allowed in stable but might be disallowed in dotnetport if it looks like an address
-                // Actually, let's try something that is definitely a valid name in stable but potentially invalid in Excel
+#if Core
+                // In dotnetport (.NET 9), names that look like cell addresses are disallowed.
+                try
+                {
+                    ws.Tables.Add(range, "A1");
+                    Assert.Fail("Should have thrown ArgumentException for name 'A1'");
+                }
+                catch (ArgumentException ex)
+                {
+                    Assert.AreEqual("Tablename is not valid", ex.Message);
+                }
+                
+                // A valid name should still work
+                ws.Tables.Add(range, "MyTable");
+                Assert.AreEqual("MyTable", ws.Tables[0].Name);
+#else
+                // In stable (Mono), "A1" is allowed as a table name.
                 ws.Tables.Add(range, "A1");
                 Assert.AreEqual("A1", ws.Tables[0].Name);
+#endif
             }
         }
 
