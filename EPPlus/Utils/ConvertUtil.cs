77845f8ae2ee0b57f99292787b2c0e522ef47e7a
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-18 15:36:34 +0000

    Add tests for ConvertUtil and implement GetTypedCellValue

--- EPPlus/Utils/ConvertUtil.cs
+++ EPPlus/Utils/ConvertUtil.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
@@ -234,9 +234,78 @@ namespace OfficeOpenXml.Utils
             return ret.ToString();
         }
 
+        /// <summary>
+        ///     Convert cell value to desired type, including nullable structs.
+        ///     When converting blank string to nullable struct (e.g. ' ' to int?) null is returned.
+        ///     When attempted conversion fails exception is passed through.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     The type to convert to.
+        /// </typeparam>
+        /// <returns>
+        ///     The <paramref name="value"/> converted to <typeparamref name="T"/>.
+        /// </returns>
+        /// <remarks>
+        ///     If input is string, parsing is performed for output types of DateTime and TimeSpan, which if fails throws <see cref="FormatException"/>.
+        ///     Another special case for output types of DateTime and TimeSpan is when input is double, in which case <see cref="DateTime.FromOADate"/>
+        ///     is used for conversion. This special case does not work through other types convertible to double (e.g. integer or string with number).
+        ///     In all other cases 'direct' conversion <see cref="Convert.ChangeType(object, Type)"/> is performed.
+        /// </remarks>
+        /// <exception cref="FormatException">
+        ///     <paramref name="value"/> is string and its format is invalid for conversion (parsing fails)
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        ///     <paramref name="value"/> is not string and direct conversion fails
+        /// </exception>
+        public static T GetTypedCellValue<T>(object value)
+        {
+            if (value == null)
+                return default(T);
+
+            var fromType = value.GetType();
+            var toType = typeof(T);
+            var toNullableUnderlyingType = (toType.IsGenericType && toType.GetGenericTypeDefinition() == typeof(Nullable<>))
+                ? Nullable.GetUnderlyingType(toType)
+                : null;
+
+            if (fromType == toType || fromType == toNullableUnderlyingType)
+                return (T)value;
+
+            // if converting to nullable struct and input is blank string, return null
+            if (toNullableUnderlyingType != null && fromType == typeof(string) && ((string)value).Trim() == string.Empty)
+                return default(T);
+
+            toType = toNullableUnderlyingType ?? toType;
+
+            if (toType == typeof(DateTime))
+            {
+                if (value is double)
+                    return (T)(object)(DateTime.FromOADate((double)value));
+
+                if (value is TimeSpan)
+                    return ((T)(object)(new DateTime(((TimeSpan)value).Ticks)));
+
+                if (value is string)
+                    return (T)(object)DateTime.Parse(value.ToString());
+            }
+            else if (toType == typeof(TimeSpan))
+            {
+                if (value is double)
+                    return (T)(object)(new TimeSpan(DateTime.FromOADate((double)value).Ticks));
+
+                if (value is DateTime)
+                    return ((T)(object)(new TimeSpan(((DateTime)value).Ticks)));
+
+                if (value is string)
+                    return (T)(object)TimeSpan.Parse(value.ToString());
+            }
+
+            return (T)Convert.ChangeType(value, toType);
+        }
+
         #region internal cache objects
         internal static TextInfo _invariantTextInfo = CultureInfo.InvariantCulture.TextInfo;
-        internal static CompareInfo _invariantCompareInfo = CompareInfo.GetCompareInfo(CultureInfo.InvariantCulture.LCID);
+        internal static CompareInfo _invariantCompareInfo = CompareInfo.GetCompareInfo(CultureInfo.InvariantCulture.Name);
         #endregion
     }
 }
--- EPPlusTest/Utils/ConvertUtilTest.cs
+++ EPPlusTest/Utils/ConvertUtilTest.cs
@@ -1,77 +1,134 @@
-﻿using System;
+using System;
 using System.Globalization;
+using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OfficeOpenXml.Utils;
 
 namespace EPPlusTest.Utils
 {
-	[TestClass]
-	public class ConvertUtilTest
-	{
-		[TestMethod]
-		public void TryParseNumericString()
-		{
-			double result;
-			object numericString = null;
-			double expected = 0;
-			Assert.IsFalse(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-			expected = 1442.0;
-			numericString = expected.ToString("e", CultureInfo.CurrentCulture); // 1.442E+003
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-			numericString = expected.ToString("f0", CultureInfo.CurrentCulture); // 1442
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-			numericString = expected.ToString("f2", CultureInfo.CurrentCulture); // 1442.00
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-			numericString = expected.ToString("n", CultureInfo.CurrentCulture); // 1,442.0
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-			expected = -0.00526;
-			numericString = expected.ToString("e", CultureInfo.CurrentCulture); // -5.26E-003
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-			numericString = expected.ToString("f0", CultureInfo.CurrentCulture); // -0
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(0.0, result);
-			numericString = expected.ToString("f3", CultureInfo.CurrentCulture); // -0.005
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(-0.005, result);
-			numericString = expected.ToString("n6", CultureInfo.CurrentCulture); // -0.005260
-			Assert.IsTrue(ConvertUtil.TryParseNumericString(numericString, out result));
-			Assert.AreEqual(expected, result);
-		}
-		
-		[TestMethod]
-		public void TryParseDateString()
-		{
-			DateTime result;
-			object dateString = null;
-			DateTime expected = DateTime.MinValue;
-			Assert.IsFalse(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(expected, result);
-			expected = new DateTime(2013, 1, 15);
-			dateString = expected.ToString("d", CultureInfo.CurrentCulture); // 1/15/2013
-			Assert.IsTrue(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(expected, result);
-			dateString = expected.ToString("D", CultureInfo.CurrentCulture); // Tuesday, January 15, 2013
-			Assert.IsTrue(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(expected, result);
-			dateString = expected.ToString("F", CultureInfo.CurrentCulture); // Tuesday, January 15, 2013 12:00:00 AM
-			Assert.IsTrue(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(expected, result);
-			dateString = expected.ToString("g", CultureInfo.CurrentCulture); // 1/15/2013 12:00 AM
-			Assert.IsTrue(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(expected, result);
-			expected = new DateTime(2013, 1, 15, 15, 26, 32);
-			dateString = expected.ToString("F", CultureInfo.CurrentCulture); // Tuesday, January 15, 2013 3:26:32 PM
-			Assert.IsTrue(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(expected, result);
-			dateString = expected.ToString("g", CultureInfo.CurrentCulture); // 1/15/2013 3:26 PM
-			Assert.IsTrue(ConvertUtil.TryParseDateString(dateString, out result));
-			Assert.AreEqual(new DateTime(2013, 1, 15, 15, 26, 0), result);
-		}
-	}
+    [TestClass]
+    public class ConvertUtilTest
+    {
+        private static MethodInfo GetMethod(string name)
+        {
+            return typeof(ConvertUtil).GetMethod(name, BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        }
+
+        private static object Invoke(string name, params object[] args)
+        {
+            var method = GetMethod(name);
+            if (method == null) throw new Exception("Method not found: " + name);
+            return method.Invoke(null, args);
+        }
+
+        [TestMethod]
+        public void TryParseNumericString()
+        {
+            var method = typeof(ConvertUtil).GetMethod("TryParseNumericString", BindingFlags.Static | BindingFlags.NonPublic);
+            
+            double result;
+            object[] args = new object[] { null, 0.0 };
+            bool success = (bool)method.Invoke(null, args);
+            Assert.IsFalse(success);
+            Assert.AreEqual(0.0, (double)args[1]);
+
+            double expected = 1442.0;
+            args[0] = expected.ToString("e", CultureInfo.CurrentCulture);
+            success = (bool)method.Invoke(null, args);
+            Assert.IsTrue(success);
+            Assert.AreEqual(expected, (double)args[1]);
+        }
+
+        [TestMethod]
+        public void TryParseDateString()
+        {
+            var method = typeof(ConvertUtil).GetMethod("TryParseDateString", BindingFlags.Static | BindingFlags.NonPublic);
+            
+            DateTime result;
+            object[] args = new object[] { null, DateTime.MinValue };
+            bool success = (bool)method.Invoke(null, args);
+            Assert.IsFalse(success);
+            Assert.AreEqual(DateTime.MinValue, (DateTime)args[1]);
+
+            DateTime expected = new DateTime(2013, 1, 15);
+            args[0] = expected.ToString("d", CultureInfo.CurrentCulture);
+            success = (bool)method.Invoke(null, args);
+            Assert.IsTrue(success);
+            Assert.AreEqual(expected, (DateTime)args[1]);
+        }
+
+        [TestMethod]
+        public void IsNumericTest()
+        {
+            Assert.IsTrue((bool)Invoke("IsNumeric", 1));
+            Assert.IsTrue((bool)Invoke("IsNumeric", 1.0));
+            Assert.IsTrue((bool)Invoke("IsNumeric", 1.0m));
+            Assert.IsTrue((bool)Invoke("IsNumeric", DateTime.Now));
+            Assert.IsTrue((bool)Invoke("IsNumeric", TimeSpan.Zero));
+            Assert.IsTrue((bool)Invoke("IsNumeric", 1L));
+            Assert.IsFalse((bool)Invoke("IsNumeric", "1"));
+            Assert.IsFalse((bool)Invoke("IsNumeric", (object)null));
+        }
+
+        private T CallGetTypedCellValue<T>(object value)
+        {
+            var method = typeof(ConvertUtil).GetMethod("GetTypedCellValue", BindingFlags.Static | BindingFlags.Public);
+            var genericMethod = method.MakeGenericMethod(typeof(T));
+            return (T)genericMethod.Invoke(null, new object[] { value });
+        }
+
+        [TestMethod]
+        public void GetTypedCellValue_BasicTypes()
+        {
+            Assert.AreEqual(1, CallGetTypedCellValue<int>(1));
+            Assert.AreEqual(1.0, CallGetTypedCellValue<double>(1.0));
+            Assert.AreEqual(1.0m, CallGetTypedCellValue<decimal>(1.0m));
+        }
+
+        [TestMethod]
+        public void GetTypedCellValue_Nullable()
+        {
+            Assert.AreEqual(1, CallGetTypedCellValue<int?>(1));
+            Assert.IsNull(CallGetTypedCellValue<int?>(null));
+            Assert.IsNull(CallGetTypedCellValue<int?>(" "));
+        }
+
+        [TestMethod]
+        public void GetTypedCellValue_DateTime()
+        {
+            var now = new DateTime(2026, 5, 18, 12, 0, 0);
+            Assert.AreEqual(now, CallGetTypedCellValue<DateTime>(now));
+            
+            // Double to DateTime
+            double oaDate = now.ToOADate();
+            Assert.AreEqual(now, CallGetTypedCellValue<DateTime>(oaDate));
+            
+            // TimeSpan to DateTime
+            var ts = TimeSpan.FromHours(1);
+            Assert.AreEqual(new DateTime(ts.Ticks), CallGetTypedCellValue<DateTime>(ts));
+        }
+
+        [TestMethod]
+        public void GetTypedCellValue_TimeSpan()
+        {
+            var ts = TimeSpan.FromHours(1);
+            Assert.AreEqual(ts, CallGetTypedCellValue<TimeSpan>(ts));
+            
+            double oaTs = 0.5; // 12 hours
+            var expectedTsFromOa = new TimeSpan(DateTime.FromOADate(0.5).Ticks);
+            Assert.AreEqual(expectedTsFromOa, CallGetTypedCellValue<TimeSpan>(oaTs));
+            
+            var dtFromTs = new DateTime(ts.Ticks);
+            Assert.AreEqual(ts, CallGetTypedCellValue<TimeSpan>(dtFromTs));
+        }
+
+        [TestMethod]
+        public void InvariantCompareInfoTest()
+        {
+            var field = typeof(ConvertUtil).GetField("_invariantCompareInfo", BindingFlags.Static | BindingFlags.NonPublic);
+            var ci = (CompareInfo)field.GetValue(null);
+            Assert.IsNotNull(ci);
+            Assert.AreEqual("", ci.Name);
+        }
+    }
 }
