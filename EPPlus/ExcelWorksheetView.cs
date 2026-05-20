28d42606a7a783581dd420c40bc27f0405963340
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-20 13:22:27 +0000

    Fix tab selection regression and verify ExcelWorksheetView differences

--- EPPlus/ExcelWorksheetView.cs
+++ EPPlus/ExcelWorksheetView.cs
@@ -314,8 +314,12 @@ namespace OfficeOpenXml
                 {
                     //    // ensure no other worksheet has its tabSelected attribute set to 1
                     foreach (ExcelWorksheet sheet in _worksheet._package.Workbook.Worksheets)
-                        sheet.View.TabSelected = false;
-
+                    {
+                        if (sheet != _worksheet)
+                        {
+                            sheet.View.TabSelected = false;
+                        }
+                    }
                 }
                 XmlElement bookView = _worksheet.Workbook.WorkbookXml.SelectSingleNode("//d:workbookView", _worksheet.NameSpaceManager) as XmlElement;
                 if (bookView != null)
--- EPPlusTest/ExcelWorksheetViewDivergenceTest.cs
+++ EPPlusTest/ExcelWorksheetViewDivergenceTest.cs
@@ -22,8 +22,26 @@ namespace EPPlusTest
 
                 ws2.View.TabSelected = true;
                 Assert.IsTrue(ws2.View.TabSelected);
-                Assert.IsFalse(ws1.View.TabSelected, "Setting TabSelected=true on Sheet2 should deselect Sheet1 in stable");
+                Assert.IsFalse(ws1.View.TabSelected, "Setting TabSelected=true on Sheet2 should deselect Sheet1");
             }
         }
+
+#if Core
+        [TestMethod]
+        public void TestTabSelectedMulti()
+        {
+            using (var pck = new ExcelPackage())
+            {
+                var ws1 = pck.Workbook.Worksheets.Add("Sheet1");
+                var ws2 = pck.Workbook.Worksheets.Add("Sheet2");
+
+                ws1.View.TabSelected = true;
+                ws2.View.TabSelectedMulti = true;
+                
+                Assert.IsTrue(ws1.View.TabSelected, "Sheet1 should still be selected");
+                Assert.IsTrue(ws2.View.TabSelected, "Sheet2 should also be selected");
+            }
+        }
+#endif
     }
 }
