4d58e96dae1af54cad4fa655240bafc8d5d42ddf
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-20 13:35:43 +0000

    Add tests for ExcelDoubleCellValue.GetHashCode and fix implementation

--- EPPlus/FormulaParsing/Excel/Functions/ExcelDoubleCellValue.cs
+++ EPPlus/FormulaParsing/Excel/Functions/ExcelDoubleCellValue.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -51,7 +51,10 @@ namespace OfficeOpenXml.FormulaParsing.Excel.Functions
         {
             return CompareTo(obj) == 0;
         }
-
+        public override int GetHashCode()
+        {
+            return Value.GetHashCode();
+        }        
         public static bool operator ==(ExcelDoubleCellValue a, ExcelDoubleCellValue b)
         {
             return a.Value.CompareTo(b.Value) == 0;
