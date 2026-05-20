80c18b34ae8b3e0fdd744b903c0b306c0951716b
Koustubh Moharir <koustubhmoharir@quantumphinance.com>
2026-05-20 13:19:48 +0000

    Add baseline tests for ExcelWorksheetView behavior

---
+++ EPPlusTest/ExcelWorksheetViewDivergenceTest.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OfficeOpenXml;
+
+namespace EPPlusTest
+{
+    [TestClass]
+    public class ExcelWorksheetViewDivergenceTest
+    {
+        [TestMethod]
+        public void TestTabSelectedBaseline()
+        {
+            using (var pck = new ExcelPackage())
+            {
+                var ws1 = pck.Workbook.Worksheets.Add("Sheet1");
+                var ws2 = pck.Workbook.Worksheets.Add("Sheet2");
+
+                ws1.View.TabSelected = true;
+                Assert.IsTrue(ws1.View.TabSelected);
+                Assert.IsFalse(ws2.View.TabSelected);
+
+                ws2.View.TabSelected = true;
+                Assert.IsTrue(ws2.View.TabSelected);
+                Assert.IsFalse(ws1.View.TabSelected, "Setting TabSelected=true on Sheet2 should deselect Sheet1 in stable");
+            }
+        }
+    }
+}
