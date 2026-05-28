d7ea9d7e1c3c938c1dee41eff2a54932af5b7d92
Koustubh Moharir <koustubhmoharir@gmail.com>
2026-05-28 20:38:37 +0000

    Count ZIP descriptor trailer only once

--- EPPlus/Packaging/DotNetZip/ZipEntry.Read.cs
+++ EPPlus/Packaging/DotNetZip/ZipEntry.Read.cs
@@ -217,7 +217,6 @@ namespace OfficeOpenXml.Packaging.Ionic.Zip
                         ze._UncompressedSize = BitConverter.ToInt64(block, i);
                         i += 8;
 
-                        ze._LengthOfTrailer += 24;  // bytes including sig, CRC, Comp and Uncomp sizes
                     }
                     else
                     {
@@ -235,11 +234,14 @@ namespace OfficeOpenXml.Packaging.Ionic.Zip
                         ze._CompressedSize = (uint)(block[i++] + block[i++] * 256 + block[i++] * 256 * 256 + block[i++] * 256 * 256 * 256);
                         ze._UncompressedSize = (uint)(block[i++] + block[i++] * 256 + block[i++] * 256 * 256 + block[i++] * 256 * 256 * 256);
 
-                        ze._LengthOfTrailer += 16;  // bytes including sig, CRC, Comp and Uncomp sizes
-
                     }
 
                     wantMore = (SizeOfDataRead != ze._CompressedSize);
+
+                    if (!wantMore)
+                    {
+                        ze._LengthOfTrailer += ze._InputUsesZip64 ? 24 : 16;  // bytes including sig, CRC, Comp and Uncomp sizes
+                    }
 
                     if (wantMore)
                     {
