200f4896e19f662446098daca57835685c01ad5a
Koustubh Moharir <koustubhmoharir@gmail.com>
2026-05-29 18:39:38 +0530

    Fixed bug in construction of address

--- EPPlus/ExcelAddress.cs
+++ EPPlus/ExcelAddress.cs
@@ -369,7 +369,8 @@ namespace OfficeOpenXml
             {
                 adr += _ws;
             }
-            adr = "'" + adr.Replace("'", "''") + "'!";
+			if (adr.Length > 0)
+				adr = "'" + adr.Replace("'", "''") + "'!";
             if (IsName)
               adr += GetAddress(_fromRow, _fromCol, _toRow, _toCol);
             else
